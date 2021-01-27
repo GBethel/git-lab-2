9d6390be080b3faec0cc3380089dda86f8dc4a6e
Garrett Bethel <gb578718@ohio.edu>
2021-01-26 20:09:09 -0500

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+*        File: git-lab-program.cc
+*      Author: Garrett Bethel
+*        Date: 1/26/21
+* Description: first program
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
